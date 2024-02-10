f0e7d9a80deac430efc707d1d3bfbb5cfb6c0812
AsadullahSamo <asadsamoon8@gmail.com>
2024-02-10 14:04:52 +0500

    Add bds element to homepage

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     
     <>
+    <bds></bds>
     <Head>
       <title> Homepage </title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
