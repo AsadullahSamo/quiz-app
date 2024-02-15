d41b9803da07efca581abdd75a43a0004973039b
AsadullahSamo <asadsamoon8@gmail.com>
2024-02-15 21:05:28 +0500

    Fix production rendering link

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-module.exports = dev ? 'http://localhost:3000' : 'https://quiz-app-omega.vercel.app'
+module.exports = dev ? 'http://localhost:3000' : 'https://quiz-app-ten-coral.vercel.app/'
 
     
